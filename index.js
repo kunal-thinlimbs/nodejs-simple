49070948ef88823f19698fd761d5cea1556c5cba
kunal patley <kunal@lordshire.com>
2020-02-07 15:43:52 +0530

    routes setup

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const port = process.env.PORT || 5500;
 app.listen(port, () => {
   console.log("app is running at", port);
 });
+
+app.get("/", (req, res) => {
+  res.status(200).json({ MSg: "hello", AppType: "Server " });
+});
+
+app.post("/post", (req, res) => {
+  res.status(200).json({ sss: "you can " });
+});
